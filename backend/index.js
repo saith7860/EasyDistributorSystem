eac668144c64ebfc2659fab7782910eae7e47b7d
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-03-07 19:51:15 +0500

    Initialize a simple backend server

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors'
+const app=express();
+app.use(cors());
+const PORT=process.env.PORT;
+app.listen(PORT,()=>{
+ console.log('app is listening on port ',PORT);
+ 
+})
