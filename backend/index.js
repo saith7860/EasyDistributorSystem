3c48df67844f75ab0e9f811d1229bd647531dc07
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2026-03-08 11:23:55 +0500

    port coming undefined is resolved in index file

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,8 @@ dotenv.config();
 import cors from 'cors'
 const app=express();
 app.use(cors());
-const PORT=process.env.PORT;
+const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
- console.log('app is listening on port ',PORT);
+ console.log('app is listening on port',PORT);
  
 })
